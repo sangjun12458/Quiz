3bf512b29df7619d18830169034c5257b75d7bb9
sangjun12458 <55080270+sangjun12458@users.noreply.github.com>
2022-01-09 15:55:25 +0900

    Create 1629_DandQ_power.cpp

---
+++ 1629_DandQ_power.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long A, B, C;
+long long ans = 1;
+
+void input()
+{
+    cin >> A >> B >> C;
+}
+void pow(long long p)
+{
+    if (p == 1)
+        ans = (ans % C) * (A % C);
+    else
+    {
+        pow(p / 2);
+        ans = (ans % C) * (ans % C);
+        if (p % 2 == 1)
+            ans = (ans % C) * (A % C);
+    }
+}
+int main()
+{
+    input();
+    pow(B);
+    cout << ans % C;
+
+    return 0;
+}
